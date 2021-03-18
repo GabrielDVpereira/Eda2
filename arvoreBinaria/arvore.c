ecbc643ba55e12e9b2d6d8646488c8a9bcd6ae7c
Gabriel Davi <gdavi.gd@gmail.com>
2021-03-18 13:15:03 -0300

    final changes binary tree

--- arvoreBinaria/arvore.c
+++ arvoreBinaria/arvore.c
@@ -34,6 +34,23 @@ NO *adiciona(NO *raiz, NO *no)
   return raiz;
 }
 
+NO *contem(TIPOCHAVE ch, NO *raiz)
+{
+  if (raiz == NULL)
+    return NULL;
+  if (raiz->chave == ch)
+    return raiz;
+
+  if (raiz->chave > ch)
+  {
+    return contem(ch, raiz->esq);
+  }
+  else
+  {
+    return contem(ch, raiz->dir);
+  }
+}
+
 NO *criaNovoNO(TIPOCHAVE ch)
 {
   NO *novoNo = (NO *)malloc(sizeof(NO));
@@ -41,6 +58,87 @@ NO *criaNovoNO(TIPOCHAVE ch)
   novoNo->esq = NULL;
   novoNo->dir = NULL;
   return novoNo;
+}
+
+int numeroNos(NO *raiz)
+{
+  if (!raiz)
+    return 0;
+  return numeroNos(raiz->esq) + 1 + numeroNos(raiz->dir);
+}
+
+void exibirArvore(NO *raiz)
+{
+  if (raiz != NULL)
+  {
+    printf("%i", raiz->chave);
+    printf("(");
+    exibirArvore(raiz->esq);
+    exibirArvore(raiz->dir);
+    printf(")");
+  }
+}
+
+NO *buscaNO(NO *raiz, TIPOCHAVE ch, NO *pai)
+{
+  NO *atual = raiz;
+  pai = NULL;
+  while (atual)
+  {
+    if (atual->chave == ch)
+      return NULL;
+    pai = atual;
+    if (ch < atual->chave)
+      atual = atual->esq;
+    else
+      atual = atual->dir;
+  }
+
+  return NULL;
+}
+
+NO *removeNo(NO *raiz, TIPOCHAVE ch)
+{
+  NO *pai, *no, *p, *q;
+  no = buscaNO(raiz, ch, &pai);
+  if (no == NULL)
+    return raiz;
+  if (!no->esq || !no->dir)
+  {
+    if (!no->esq)
+      q = no->dir;
+    else
+      q = no->esq;
+  }
+  else
+  {
+    p = no;
+    q = no->esq;
+    while (q->dir)
+    {
+      p = q;
+      q = q->dir;
+    }
+
+    if (p != no)
+    {
+      p->dir = q->esq;
+      p->esq = no->esq;
+    }
+    q->dir = no->dir;
+  }
+  if (!pai)
+  {
+    free(no);
+    free(q);
+  }
+
+  if (ch < pai->chave)
+    pai->esq = q;
+  else
+    pai->dir = q;
+  free(no);
+  return raiz;
 }
 
 int main()
@@ -53,4 +151,8 @@ int main()
   NO *no2 = criaNovoNO(12);
 
   r = adiciona(r, no2);
+
+  // NO *p = contem(12, r);
+  // printf("%d", numeroNos(r));
+  exibirArvore(r);
 }
